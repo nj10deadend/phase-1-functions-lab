326e6d75f2b3fb2c518fd8c7aac0c142fe0bda0f
nj10deadend <nasjacks10@gmail.com>
2021-07-20 12:05:58 -0400

    commit tree f6d1dc7e43325a6221ff4e4d2fd70e1dd7543e19

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    let scuberHq = 42;
+    return Math.abs(location - scuberHq);
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(location) {
+    let scuberHq = 42;
+    return Math.abs((location - scuberHq) * 264);
+}
+distanceFromHqInFeet();
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs((start - end) * 264);
+}
+distanceTravelledInFeet(48, 43);
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start, end)
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance >400 && distance <=2000) {
+        return 0.02 * (distance-400);
+    } else if (distance >2000 && distance <=2500) {
+        return 25; 
+    } else if (distance >2500) {
+        return 'cannot travel that far'
+    }
+}
